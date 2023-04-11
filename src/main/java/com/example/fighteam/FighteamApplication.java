2d5fe8a0d08a738d35d5335f7238dd08189847d4
KIMHYOJUN <olzitoon@naver.com>
2023-04-11 11:40:49 +0900

    Feat. Project init

---
+++ src/main/java/com/example/fighteam/FighteamApplication.java
@@ -0,0 +1,13 @@
+package com.example.fighteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FighteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FighteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fighteam/FighteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fighteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FighteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
