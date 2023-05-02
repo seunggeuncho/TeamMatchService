16c159dc7bdabba1dea7cf89b985652100356510
KIMHYOJUN <olzitoon@naver.com>
2023-05-02 11:43:18 +0900

    Feat: 채팅방 CRUD 기능 구현

--- src/main/java/com/example/fighteam/chat/service/ChatRoomService.java
+++ src/main/java/com/example/fighteam/chat/service/ChatRoomService.java
@@ -1,6 +1,7 @@
 package com.example.fighteam.chat.service;
 
-import com.example.fighteam.chat.domain.dto.ChatMessageRequestDto;
+import com.example.fighteam.chat.domain.dto.ChatMessageResponseDto;
+import com.example.fighteam.chat.domain.repository.ChatMessage;
 import com.example.fighteam.chat.domain.repository.ChatRoom;
 import com.example.fighteam.chat.domain.repository.ChatRoomRepository;
 import com.example.fighteam.user.domain.repository.User;
@@ -8,17 +9,20 @@ import com.example.fighteam.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class ChatService {
+public class ChatRoomService {
     private final UserRepository userRepository;
     private final ChatRoomRepository chatRoomRepository;
 
+    @Transactional
     public Long createChat(Long[] users) {
         List<User> chatMember = userRepository.findAllById(Arrays.asList(users));
         ChatRoom createdChatRoom = new ChatRoom(chatMember.get(0), chatMember.get(1));
@@ -26,8 +30,21 @@ public class ChatService {
         return createdChatRoom.getId();
     }
 
-    public void sendMessage(ChatMessageRequestDto chatMessageRequestDto) {
+    @Transactional(readOnly = true)
+    public ChatRoom roomInfoReturn(Long roomId) {
+        return chatRoomRepository.findById(roomId).orElse(null);
+    }
 
+    @Transactional(readOnly = true)
+    public List<ChatMessageResponseDto> messageInfoReturn(Long roomId) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElse(null);
+        List<ChatMessage> messages = chatRoom.getMessages();
+        List<ChatMessageResponseDto> result = new ArrayList<>();
+        for (ChatMessage c : messages) {
+            result.add(new ChatMessageResponseDto(String.valueOf(c.getSender().getId()), c.getMessage(), String.valueOf(roomId)));
+        }
+
+        return result;
     }
 
 }
