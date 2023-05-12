6345ed03392c23f589af75c053549354b0666975
Hwang-JeongHwan <jeonghwan97@naver.com>
2023-05-12 10:44:58 +0900

    Feat: 엔티티 구현

--- src/main/java/com/example/fighteam/payment/domain/Post.java
+++ src/main/java/com/example/fighteam/payment/domain/Post.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Setter
 @Getter
@@ -16,6 +18,14 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
-
+    private String title;
+    private String content;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private int recruitDate;
     private int postDeposit;
+    private LocalDateTime date;
+    private int count;
+    private Boolean complete;
+    private String subject;
 }
