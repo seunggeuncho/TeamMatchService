86af286549e0ba9eaa8e1178857ad2a517b9db8c
KIMHYOJUN <olzitoon@naver.com>
2023-05-12 22:36:12 +0900

    Feat: 매핑관계 추가

--- src/main/java/com/example/fighteam/user/domain/repository/User.java
+++ src/main/java/com/example/fighteam/user/domain/repository/User.java
@@ -1,11 +1,14 @@
 package com.example.fighteam.user.domain.repository;
 
+import com.example.fighteam.chat.domain.repository.ChatRoom;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -26,6 +29,9 @@ public class User {
 
     private String tel; // 전화번호
     private String role;        //권한을 어디서 줄지 안정했지만 일단은 관리자와 구분하기위해 추가했습니다.
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatRoom> roomList = new ArrayList<>();
 
     @Builder
     public User(Long id, String email, String passwd, String name, String tel) {
