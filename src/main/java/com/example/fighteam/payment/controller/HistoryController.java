57d05b0551a3cdd23a27cc1900d72f2eb1904e1b
Hwang-JeongHwan <jeonghwan97@naver.com>
2023-05-12 10:48:21 +0900

    Chore: System.out.print 전부 제거

--- src/main/java/com/example/fighteam/payment/controller/HistoryController.java
+++ src/main/java/com/example/fighteam/payment/controller/HistoryController.java
@@ -33,14 +33,14 @@ public class HistoryController {
     public String getHistoryList(@RequestParam("type") String type, Model model, HttpSession session) {
 
         Long id = (Long) session.getAttribute("loginId");
-        System.out.println("id = " + id);
-        System.out.println("type = " + type);
+//        System.out.println("id = " + id);
+//        System.out.println("type = " + type);
         List<History> findHistory = historyRepository.findByMemberId(id, type);
-        for (History history : findHistory) {
-            System.out.println("history.getDate() = " + history.getDate());
-            System.out.println("history.getCost() = " + history.getCost());
-            System.out.println("history.getBalance() = " + history.getBalance());
-        }
+//        for (History history : findHistory) {
+//            System.out.println("history.getDate() = " + history.getDate());
+//            System.out.println("history.getCost() = " + history.getCost());
+//            System.out.println("history.getBalance() = " + history.getBalance());
+//        }
         model.addAttribute("historyList", findHistory);
         return "hwang/history/historyList";
     }
