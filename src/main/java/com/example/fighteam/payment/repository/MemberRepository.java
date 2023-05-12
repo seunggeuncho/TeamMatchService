084058f8522f91d139b9a7f9148062a14c930915
Hwang-JeongHwan <jeonghwan97@naver.com>
2023-05-12 10:45:31 +0900

    Chore: System.out.print 전부 제거

--- src/main/java/com/example/fighteam/payment/repository/MemberRepository.java
+++ src/main/java/com/example/fighteam/payment/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package com.example.fighteam.payment.repository;
 
 import com.example.fighteam.payment.domain.Member;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class MemberRepository {
