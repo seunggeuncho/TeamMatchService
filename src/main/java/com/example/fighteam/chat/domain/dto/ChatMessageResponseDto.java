a98a9ed0a1e36c0f1001dc66d0fea1049bbcbb58
KIMHYOJUN <olzitoon@naver.com>
2023-05-13 11:07:11 +0900

    Feat: 메시지 이름 추가

--- src/main/java/com/example/fighteam/chat/domain/dto/ChatMessageResponseDto.java
+++ src/main/java/com/example/fighteam/chat/domain/dto/ChatMessageResponseDto.java
@@ -13,10 +13,12 @@ public class ChatMessageResponseDto {
     private String sender;
     private String message;
     private String roomId;
+    private String senderName;
 
-    public ChatMessageResponseDto(String sender, String message, String roomId) {
+    public ChatMessageResponseDto(String sender, String message, String roomId, String senderName) {
         this.sender = sender;
         this.message = message;
         this.roomId = roomId;
+        this.senderName = senderName;
     }
 }
